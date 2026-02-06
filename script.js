7b4def95ee73db0c48e442d8f4501b1c32ca38d8
Pearl <itsprlscnt@gmail.com>
2026-02-05 20:17:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ function playSound() {
     audio.play();
 }
 
-document.getElementById("yes-button").addEventListener("click", playSound);
+document.getElementById("yes-button").addEventListener("click", function() {
+    playSound();
+
+    document.getElementById("yes-button").style.display = "none";
+    document.getElementById("no-button").style.display = "none";
+    document.getElementById("question").style.display = "none";
+    document.getElementById("bear").style.display = "none";
+
+    document.getElementById("hearts").style.display = "block";
+    document.getElementById("answer").style.display = "block";
+});
